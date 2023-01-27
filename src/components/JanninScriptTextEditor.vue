f281e851e7c5a98201fd1d4ca7729b068794cb0c
Jordan Landry <jordanlandry1999@gmail.com>
2023-01-27 15:01:20 -0500

    Initial commit

---
+++ src/components/JanninScriptTextEditor.vue
@@ -0,0 +1,17 @@
+import { defineComponent } from "vue";
+
+<template>
+  <h1>HELLO!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "JanninScriptTextEditor",
+  props: {
+    msg: String,
+  },
+});
+</script>
+<style scoped></style>
